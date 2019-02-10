268d0be4942b69e4ae14e58b7d1dbb7adc87d7a7
Roman Patrushev <banan4eg1989@gmail.com>
2019-02-10 15:55:36 +0300

    +

--- home_work_05/src/main/java/com/patrushev/my_testing_framework/MyTestingFramework.java
+++ home_work_05/src/main/java/com/patrushev/my_testing_framework/MyTestingFramework.java
@@ -10,36 +10,49 @@ import java.util.List;
 
 @SuppressWarnings("WeakerAccess")
 public class MyTestingFramework {
-    private static List<Method> beforeAllMethods = new ArrayList<>();
-    private static List<Method> beforeEachMethods = new ArrayList<>();
-    private static List<Method> testMethods = new ArrayList<>();
-    private static List<Method> afterEachMethods = new ArrayList<>();
-    private static List<Method> afterAllMethods = new ArrayList<>();
-
-    private static int successfulTestsCount;
 
     public static <T> void runTests(Class<T> testingClass) {
+        List<Method> beforeAllMethods = new ArrayList<>();
+        List<Method> beforeEachMethods = new ArrayList<>();
+        List<Method> testMethods = new ArrayList<>();
+        List<Method> afterEachMethods = new ArrayList<>();
+        List<Method> afterAllMethods = new ArrayList<>();
+        int successfulTestsCount = 0;
         try {
             //получаем конструктор переданного класса
             Constructor<T> constructor = testingClass.getDeclaredConstructor();
-            prepareTests(testingClass);
-            makePreliminaryWork();
-            makeTests(constructor);
-            makeFinalWork();
+            prepareTests(testingClass, beforeAllMethods, beforeEachMethods, testMethods, afterEachMethods, afterAllMethods);
+            makePreliminaryWork(beforeAllMethods);
+            successfulTestsCount = makeTests(constructor, beforeEachMethods, testMethods, afterEachMethods);
+            makeFinalWork(afterAllMethods);
         } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException | InstantiationException e) {
             e.printStackTrace();
         }
         System.out.println("Успешно выполнены " + successfulTestsCount + " тестов из " + testMethods.size());
     }
 
-    private static void makeFinalWork() throws IllegalAccessException, InvocationTargetException {
-        //запускаем методы AfterAll
-        for (Method afterAllMethod : afterAllMethods) {
-            afterAllMethod.invoke(null);
+    private static <T> void prepareTests(Class<T> testingClass, List<Method> beforeAllMethods, List<Method> beforeEachMethods, List<Method> testMethods, List<Method> afterEachMethods, List<Method> afterAllMethods) {
+        //получаем все методы класса
+        Method[] allMethods = testingClass.getDeclaredMethods();
+        //распределяем их в соответствии с аннотациями
+        for (Method method : allMethods) {
+            if (method.getAnnotation(BeforeAll.class) != null) beforeAllMethods.add(method);
+            if (method.getAnnotation(BeforeEach.class) != null) beforeEachMethods.add(method);
+            if (method.getAnnotation(Test.class) != null) testMethods.add(method);
+            if (method.getAnnotation(AfterEach.class) != null) afterEachMethods.add(method);
+            if (method.getAnnotation(AfterAll.class) != null) afterAllMethods.add(method);
         }
     }
 
-    private static <T> void makeTests(Constructor<T> constructor) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+    private static void makePreliminaryWork(List<Method> beforeAllMethods) throws IllegalAccessException, InvocationTargetException {
+        //вызываем методы BeforeAll
+        for (Method beforeAllMethod : beforeAllMethods) {
+            beforeAllMethod.invoke(null);
+        }
+    }
+
+    private static <T> int makeTests(Constructor<T> constructor, List<Method> beforeEachMethods, List<Method> testMethods, List<Method> afterEachMethods) throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        int successfulTestsCount = 0;
         //запускаем цикл тестов
         for (Method testMethod : testMethods) {
             //создаем новый объект тестового класса
@@ -57,7 +70,7 @@ public class MyTestingFramework {
                 if (e.getCause() instanceof AssertionError) {
                     System.out.println("Тест завершен неудачно!");
                 } else {
-                    System.out.println("Не получилось закончить тест!");
+                    System.out.println("Не удалось закончить тест!");
                     e.printStackTrace();
                 }
             }
@@ -66,26 +79,13 @@ public class MyTestingFramework {
                 afterEachMethod.invoke(testClassInstance);
             }
         }
+        return successfulTestsCount;
     }
 
-    private static void makePreliminaryWork() throws IllegalAccessException, InvocationTargetException {
-        //вызываем методы BeforeAll
-        for (Method beforeAllMethod : beforeAllMethods) {
-            beforeAllMethod.invoke(null);
-        }
-    }
-
-    private static <T> void prepareTests(Class<T> testingClass) {
-        //получаем все методы класса
-        Method[] allMethods = testingClass.getDeclaredMethods();
-
-        //распределяем их в соответствии с аннотациями
-        for (Method method : allMethods) {
-            if (method.getAnnotation(BeforeAll.class) != null) beforeAllMethods.add(method);
-            if (method.getAnnotation(BeforeEach.class) != null) beforeEachMethods.add(method);
-            if (method.getAnnotation(Test.class) != null) testMethods.add(method);
-            if (method.getAnnotation(AfterEach.class) != null) afterEachMethods.add(method);
-            if (method.getAnnotation(AfterAll.class) != null) afterAllMethods.add(method);
+    private static void makeFinalWork(List<Method> afterAllMethods) throws IllegalAccessException, InvocationTargetException {
+        //запускаем методы AfterAll
+        for (Method afterAllMethod : afterAllMethods) {
+            afterAllMethod.invoke(null);
         }
     }
 }
