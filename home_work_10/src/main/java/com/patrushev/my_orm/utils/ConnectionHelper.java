1a4e3b382e8fe60e98d57e252f4d618fdaa4089c
Roman Patrushev <banan4eg1989@gmail.com>
2019-04-16 13:47:09 +0300

    +

--- home_work_10/src/main/java/com/patrushev/my_orm/utils/ConnectionHelper.java
+++ home_work_10/src/main/java/com/patrushev/my_orm/utils/ConnectionHelper.java
@@ -3,23 +3,49 @@ package com.patrushev.my_orm.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionHelper {
     private static Logger logger = LoggerFactory.getLogger(ConnectionHelper.class);
 
+//    /**
+//     * @return подключение к базе данных "postgres"
+//     */
+//    public static Connection getPostgresqlConnection() throws SQLException {
+//        final String connString = "jdbc:postgresql://" +    // db type
+//                "localhost:" +                              // host name
+//                "5432/" +                                   // port
+//                "postgres?" +                               // db name
+//                "user=postgres";                            // login
+//        logger.info("Создано подключение к БД postgres.");
+//        return DriverManager.getConnection(connString);
+//    }
+
     /**
-     * @return подключение к базе данных "postgres"
+     * @return подключение к базе данных
      */
     public static Connection getPostgresqlConnection() throws SQLException {
-        final String connString = "jdbc:postgresql://" +    // db type
-                "localhost:" +                              // host name
-                "5432/" +                                   // port
-                "postgres?" +                               // db name
-                "user=postgres";                            // login
-        logger.info("Создано подключение к БД postgres.");
+        String connString = null;
+        try (InputStream input = new FileInputStream("src/main/resources/config.properties")) {
+            Properties prop = new Properties();
+            prop.load(input);
+            connString = "jdbc:" + prop.getProperty("db.type") + "://" +    // db type
+                    prop.getProperty("db.host") + ":" +                             // host name
+                    prop.getProperty("db.port") + "/" +                                  // port
+                    prop.getProperty("db.name") + "?" +                               // db name
+                    "user=" + prop.getProperty("db.login");                            // login
+            logger.info("Создано подключение к БД postgres запросом " + connString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert connString != null;
         return DriverManager.getConnection(connString);
     }
 }
