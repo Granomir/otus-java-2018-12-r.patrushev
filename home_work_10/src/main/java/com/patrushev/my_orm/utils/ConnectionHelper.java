7a84bf8c9f468cfc4762f54e7d7cd2be0427e97a
Roman Patrushev <banan4eg1989@gmail.com>
2019-04-16 13:48:18 +0300

    +

--- home_work_10/src/main/java/com/patrushev/my_orm/utils/ConnectionHelper.java
+++ home_work_10/src/main/java/com/patrushev/my_orm/utils/ConnectionHelper.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -14,19 +13,6 @@ import java.util.Properties;
 
 public class ConnectionHelper {
     private static Logger logger = LoggerFactory.getLogger(ConnectionHelper.class);
-
-//    /**
-//     * @return подключение к базе данных "postgres"
-//     */
-//    public static Connection getPostgresqlConnection() throws SQLException {
-//        final String connString = "jdbc:postgresql://" +    // db type
-//                "localhost:" +                              // host name
-//                "5432/" +                                   // port
-//                "postgres?" +                               // db name
-//                "user=postgres";                            // login
-//        logger.info("Создано подключение к БД postgres.");
-//        return DriverManager.getConnection(connString);
-//    }
 
     /**
      * @return подключение к базе данных
