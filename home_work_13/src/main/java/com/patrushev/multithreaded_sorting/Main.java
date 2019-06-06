172dd87e537fa28657b87f57beb66144e1f110c1
Roman Patrushev <banan4eg1989@gmail.com>
2019-06-06 12:03:07 +0300

    +

--- home_work_13/src/main/java/com/patrushev/multithreaded_sorting/Main.java
+++ home_work_13/src/main/java/com/patrushev/multithreaded_sorting/Main.java
@@ -1,4 +1,16 @@
 package com.patrushev.multithreaded_sorting;
 
+import java.util.Arrays;
+import java.util.concurrent.ForkJoinPool;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 5, 4, 32, 12, 99, 76, 11, 54};
+        long begin = System.nanoTime();
+        ForkJoinPool pool = new ForkJoinPool();
+        int[] sortedArr = pool.invoke(new Sorter(arr));
+        long end = System.nanoTime();
+        System.out.println("Сортировка заняла " + (end - begin) + " наносекунд");
+        System.out.println(Arrays.toString(sortedArr));
+    }
 }
--- home_work_13/src/main/java/com/patrushev/multithreaded_sorting/Sorter.java
+++ home_work_13/src/main/java/com/patrushev/multithreaded_sorting/Sorter.java
@@ -1,4 +1,68 @@
 package com.patrushev.multithreaded_sorting;
 
-public class Sorter {
+import java.util.concurrent.RecursiveTask;
+
+public class Sorter extends RecursiveTask<int[]> {
+
+    private int[] arrayA;
+
+    public Sorter(int[] arrayA) {
+        this.arrayA = arrayA;
+    }
+
+    @Override
+    protected int[] compute() {
+        // проверяем не нулевой ли он?
+        if (arrayA == null) {
+            return null;
+        }
+        // проверяем не 1 ли элемент в массиве?
+        if (arrayA.length < 2) {
+            return arrayA; // возврат в рекурсию в строки ниже см комменты.
+        }
+        // копируем левую часть от начала до середины
+        int[] arrayB = new int[arrayA.length / 2];
+        System.arraycopy(arrayA, 0, arrayB, 0, arrayA.length / 2);
+
+        // копируем правую часть от середины до конца массива, вычитаем из длины первую часть
+        int[] arrayC = new int[arrayA.length - arrayA.length / 2];
+        System.arraycopy(arrayA, arrayA.length / 2, arrayC, 0, arrayA.length - arrayA.length / 2);
+
+        // рекурсией закидываем поделенные обе части обратно в наш метод, он будет крутится до тех пор,
+        // пока не дойдет до 1 элемента в массиве, после чего вернется в строку и будет искать второй такой же,
+        // точнее правую часть от него и опять вернет его назад
+        final Sorter leftSorter = new Sorter(arrayB);
+        leftSorter.fork();
+        final Sorter rightSorter = new Sorter(arrayC);
+        rightSorter.fork();
+
+        arrayB = leftSorter.join(); // левая часть возврат из рекурсии строкой return arrayA;
+        arrayC = rightSorter.join(); // правая часть возврат из рекурсии строкой return arrayA;
+
+        // далее опять рекурсия возврата слияния двух отсортированных массивов
+        return mergeArray(arrayB, arrayC);
+    }
+
+    private static int[] mergeArray(int[] arrayA, int[] arrayB) {
+
+        int[] arrayC = new int[arrayA.length + arrayB.length];
+        int positionA = 0, positionB = 0;
+
+        for (int i = 0; i < arrayC.length; i++) {
+            if (positionA == arrayA.length) {               //уже прошли весь массив А
+                arrayC[i] = arrayB[positionB];          //начинаем просто копировать остаток массива В
+                positionB++;
+            } else if (positionB == arrayB.length) {        //уже прошли весь массив B
+                arrayC[i] = arrayA[positionA];          //начинаем просто копировать остаток массива A
+                positionA++;
+            } else if (arrayA[positionA] < arrayB[positionB]) {
+                arrayC[i] = arrayA[positionA];
+                positionA++;
+            } else {
+                arrayC[i] = arrayB[positionB];
+                positionB++;
+            }
+        }
+        return arrayC;
+    }
 }
