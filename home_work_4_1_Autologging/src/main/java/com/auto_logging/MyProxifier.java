5f557e2cd96c9d8a064d0ac8574380c8f39f79f9
Roman Patrushev <roman.gap@mail.ru>
2019-08-07 18:36:44 +0300

    готовое ДЗ 4.1 (автологирование)

---
+++ home_work_4_1_Autologging/src/main/java/com/auto_logging/MyProxifier.java
@@ -0,0 +1,33 @@
+package com.auto_logging;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class MyProxifier {
+
+    public static Object getProxifiedObject(Object objectForProxying) {
+        InvocationHandler invocationHandler = new MyInvocationHandler(objectForProxying);
+        return Proxy.newProxyInstance(MyProxifier.class.getClassLoader(), objectForProxying.getClass().getInterfaces(), invocationHandler);
+    }
+
+    private static class MyInvocationHandler implements InvocationHandler {
+        private Object objectForProxying;
+
+        public MyInvocationHandler(Object objectForProxying) {
+            this.objectForProxying = objectForProxying;
+        }
+
+        @Override
+        public Object invoke(Object o, Method method, Object[] args) throws Throwable {
+            if (method.getDeclaredAnnotation(Log.class) != null) {
+                System.out.print("executed method: " + method.getName() + " with params: ");
+                String params = Arrays.stream(args).map(Object::toString).collect(Collectors.joining(", "));
+                System.out.println(params);
+            }
+            return method.invoke(objectForProxying, args);
+        }
+    }
+}
