7a978e1fa620a13eb6fc11c39c9d07edc37b1901
Roman Patrushev <roman.gap@mail.ru>
2019-08-08 13:38:40 +0300

    добавил складывание методов в хэшсет при создании прокси

--- home_work_4_1_Autologging/src/main/java/com/auto_logging/MyProxifier.java
+++ home_work_4_1_Autologging/src/main/java/com/auto_logging/MyProxifier.java
@@ -4,6 +4,7 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.stream.Collectors;
 
 public class MyProxifier {
@@ -15,14 +16,18 @@ public class MyProxifier {
 
     private static class MyInvocationHandler implements InvocationHandler {
         private Object objectForProxying;
+        private HashSet<Method> annotatedMethods;
 
         public MyInvocationHandler(Object objectForProxying) {
             this.objectForProxying = objectForProxying;
+            annotatedMethods = new HashSet<>();
+            Class<?>[] interfaces = objectForProxying.getClass().getInterfaces();
+            Arrays.stream(interfaces).map(Class::getMethods).flatMap(Arrays::stream).filter(method -> method.getDeclaredAnnotation(Log.class) != null).forEach(method -> annotatedMethods.add(method));
         }
 
         @Override
         public Object invoke(Object o, Method method, Object[] args) throws Throwable {
-            if (method.getDeclaredAnnotation(Log.class) != null) {
+            if (annotatedMethods.contains(method)) {
                 System.out.print("executed method: " + method.getName() + " with params: ");
                 String params = Arrays.stream(args).map(Object::toString).collect(Collectors.joining(", "));
                 System.out.println(params);
