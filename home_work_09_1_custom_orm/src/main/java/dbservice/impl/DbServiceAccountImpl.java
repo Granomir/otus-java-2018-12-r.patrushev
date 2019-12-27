d1a15189cb4009fc3934acfb2f6adc5aa3a9624e
Roman Patrushev <rpatrushev@stream.ru>
2019-12-27 16:10:09 +0300

    @override

--- home_work_09_1_custom_orm/src/main/java/dbservice/impl/DbServiceAccountImpl.java
+++ home_work_09_1_custom_orm/src/main/java/dbservice/impl/DbServiceAccountImpl.java
@@ -12,18 +12,22 @@ public class DbServiceAccountImpl implements DBService<Account> {
         this.template = template;
     }
 
+    @Override
     public long create(Account account) {
         return template.create(account);
     }
 
+    @Override
     public void update(Account account) {
         template.update(account);
     }
 
+    @Override
     public Account load(long id) {
         return template.load(id, Account.class);
     }
 
+    @Override
     public long createOrUpdate(Account account) {
         return template.createOrUpdate(account);
     }
--- home_work_09_1_custom_orm/src/main/java/dbservice/impl/DbServiceUserImpl.java
+++ home_work_09_1_custom_orm/src/main/java/dbservice/impl/DbServiceUserImpl.java
@@ -11,18 +11,22 @@ public class DbServiceUserImpl implements DBService<User> {
         this.template = template;
     }
 
+    @Override
     public long create(User user) {
         return template.create(user);
     }
 
+    @Override
     public void update(User user) {
         template.update(user);
     }
 
+    @Override
     public User load(long id) {
         return template.load(id, User.class);
     }
 
+    @Override
     public long createOrUpdate(User user) {
         return template.createOrUpdate(user);
     }
