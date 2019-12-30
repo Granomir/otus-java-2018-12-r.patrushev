107d2155d19035e40b964b6ce64dbea59eefa995
Roman Patrushev <rpatrushev@stream.ru>
2019-12-30 17:35:48 +0300

    убрал всю работу с рефлексией в ReflectionHelper

--- home_work_09_1_custom_orm/src/main/java/dbservice/impl/JDBCTemplateImpl.java
+++ home_work_09_1_custom_orm/src/main/java/dbservice/impl/JDBCTemplateImpl.java
@@ -1,16 +1,12 @@
 package dbservice.impl;
 
-import dbservice.JDBCTemplate;
 import dbservice.DbExecutor;
-import dbservice.Id;
+import dbservice.JDBCTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import test_datasorce.DataSource;
 import utils.ReflectionHelper;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.*;
@@ -19,7 +15,6 @@ import java.util.stream.Collectors;
 public class JDBCTemplateImpl implements JDBCTemplate {
     private Logger logger = LoggerFactory.getLogger(JDBCTemplateImpl.class);
     private DbExecutor executor;
-    private Map<Class<?>, Field> acceptableClasses;
     private Map<Class<?>, String> insertQueries;
     private DataSource dataSource;
     private Map<Class<?>, String> updateQueries;
@@ -29,7 +24,6 @@ public class JDBCTemplateImpl implements JDBCTemplate {
     public JDBCTemplateImpl(DbExecutor executor, DataSource dataSource) {
         this.executor = executor;
         this.dataSource = dataSource;
-        acceptableClasses = new HashMap<>();
         insertQueries = new HashMap<>();
         updateQueries = new HashMap<>();
         selectQueries = new HashMap<>();
@@ -40,17 +34,9 @@ public class JDBCTemplateImpl implements JDBCTemplate {
     public <T> long create(T objectData) {
         logger.info("start creating entity");
         Class<?> clazz = objectData.getClass();
-        List<Field> fields = ReflectionHelper.getAllDeclaredFieldsFromClass(clazz);
-        Field idField = getIdField(clazz, fields);
         List<String> columns = new ArrayList<>();
         List<Object> values = new ArrayList<>();
-        for (Field field : fields) {
-            String fieldName = field.getName();
-            if (!fieldName.equals(idField.getName())) {
-                columns.add(fieldName);
-                values.add(ReflectionHelper.getFieldValue(objectData, fieldName));
-            }
-        }
+        ReflectionHelper.fillFieldsNamesWithValues(objectData, columns, values);
         long id = -1;
         String sqlQuery = getInsertQuery(clazz, columns);
         logger.info("prepared jdbc template - {}", sqlQuery);
@@ -64,15 +50,6 @@ public class JDBCTemplateImpl implements JDBCTemplate {
         return id;
     }
 
-    private Field getIdField(Class<?> clazz, List<Field> fields) {
-        Field idField = acceptableClasses.get(clazz);
-        if (idField == null) {
-            idField = getIdField(fields);
-            acceptableClasses.put(clazz, idField);
-        }
-        return idField;
-    }
-
     private String getInsertQuery(Class<?> clazz, List<String> columns) {
         String query = insertQueries.get(clazz);
         if (null == query) {
@@ -82,31 +59,14 @@ public class JDBCTemplateImpl implements JDBCTemplate {
         return query;
     }
 
-    private Field getIdField(List<Field> fields) {
-        for (Field field : fields) {
-            if (field.getAnnotation(Id.class) != null) {
-                return field;
-            }
-        }
-        throw new IllegalArgumentException("DBService может работать только с классами, имеющими поле с аннотацией \"@dbservice.Id\"");
-    }
-
     @Override
     public <T> void update(T objectData) {
         logger.info("start updating entity");
         Class<?> clazz = objectData.getClass();
-        List<Field> fields = ReflectionHelper.getAllDeclaredFieldsFromClass(clazz);
-        Field idField = getIdField(clazz, fields);
         List<String> columns = new ArrayList<>();
         List<Object> values = new ArrayList<>();
-        String idFieldName = idField.getName();
-        for (Field field : fields) {
-            String fieldName = field.getName();
-            if (!fieldName.equals(idFieldName)) {
-                columns.add(fieldName);
-                values.add(ReflectionHelper.getFieldValue(objectData, fieldName));
-            }
-        }
+        ReflectionHelper.fillFieldsNamesWithValues(objectData, columns, values);
+        String idFieldName = ReflectionHelper.getIdFieldName(clazz);
         String sqlQuery = getUpdateQuery(clazz, idFieldName, columns);
         logger.info("prepared jdbc template - {}", sqlQuery);
         try (Connection connection = dataSource.getConnection()) {
@@ -130,23 +90,22 @@ public class JDBCTemplateImpl implements JDBCTemplate {
     @Override
     public <T> T load(long id, Class<T> clazz) {
         logger.info("start loading entity");
-        List<Field> fields = ReflectionHelper.getAllDeclaredFieldsFromClass(clazz);
-        Field idField = getIdField(clazz, fields);
-        String sqlQuery = getSelectQuery(clazz, idField.getName());
+        String sqlQuery = getSelectQuery(clazz, ReflectionHelper.getIdFieldName(clazz));
         logger.info("prepared jdbc template - {}", sqlQuery);
         Optional<T> loadedEntity = Optional.empty();
         try (Connection connection = dataSource.getConnection()) {
             loadedEntity = executor.selectRecord(sqlQuery, id, connection, resultSet -> {
                 try {
                     if (resultSet.next()) {
-                        Constructor<T> constructor = clazz.getConstructor();
-                        T entity = constructor.newInstance();
-                        for (Field field : fields) {
-                            ReflectionHelper.setFieldValue(entity, field, resultSet.getObject(field.getName()));
+                        T entity = ReflectionHelper.getEmptyEntity(clazz);
+                        List<String> fieldsNames = ReflectionHelper.getAllDeclaredFieldsNamesFromClass(clazz);
+                        Map<String, Object> values = new HashMap<>();
+                        for (String field : fieldsNames) {
+                            values.put(field, resultSet.getObject(field));
                         }
-                        return entity;
+                        return ReflectionHelper.fillEntity(clazz, entity, values);
                     }
-                } catch (SQLException | IllegalAccessException | NoSuchMethodException | InstantiationException | InvocationTargetException e) {
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
                 return null;
@@ -180,8 +139,7 @@ public class JDBCTemplateImpl implements JDBCTemplate {
     public <T> long createOrUpdate(T objectData) {
         logger.info("start creating or updating entity");
         Class<?> clazz = objectData.getClass();
-        Field idField = getIdField(clazz, ReflectionHelper.getAllDeclaredFieldsFromClass(clazz));
-        String idFieldName = idField.getName();
+        String idFieldName = ReflectionHelper.getIdFieldName(clazz);
         long id = (long) ReflectionHelper.getFieldValue(objectData, idFieldName);
         if (id == 0) {
             return create(objectData);
